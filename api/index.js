a0c1c04abcb0acb87b8a93af40a9a154ce785225
Anilkushwaha782000 <anilkushwaha1297@gmail.com>
2024-03-19 23:59:58 +0530

    connect to the db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app=express()
+const mongoose= require("mongoose")
+const dotenv=require('dotenv')
+dotenv.config()
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("mongodb is connected");
+}).catch((err)=>{
+    console.log("could not connect to the  mongodb");
+})
+app.get("/test",(req,res)=>{
+    res.json("hello from server side")
+})
 app.listen(3001,()=>{
     console.log("server is running on port 3001!!")
 })
